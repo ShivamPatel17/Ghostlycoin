695ec5d1f863520f7d9e872526cbc07aab7adecc
Shivam <patel.shivam.m@gmail.com>
2021-09-08 22:54:23 -0400

    done with the debugging ugh

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -8,7 +8,6 @@ module.exports = function (deployer) {
   deployer.deploy(Ghostlycoin, 1000000).then(function(){
     //Token price is 0.001 ETH
     var tokenPrice = 10000000; //in wei (0.001 ETH)
-    ; //in wei (0.001 ETH)
     return deployer.deploy(GhostlycoinSale, Ghostlycoin.address, tokenPrice);
   });
 };
